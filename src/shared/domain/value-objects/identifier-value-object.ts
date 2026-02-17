9ccacd1a37b9d0d5b264f86d7503f19aedba423d
Grecia Fuentes <greciafuentes307@gmail.com>
2026-02-17 09:55:38 -0600

    fix(identifierValueObject): change the verification for the uuid

--- src/shared/domain/value-objects/identifier-value-object.ts
+++ src/shared/domain/value-objects/identifier-value-object.ts
@@ -1,15 +1,18 @@
 import ValueObject from "./value-objects"
 
-class IdentifierValueObject extends ValueObject<string>{
+class IdentifierValueObject extends ValueObject<string> {
 
     constructor(value: string) {
         super(value)
         this.ensureValueIsUuid()
     }
 
-    private ensureValueIsUuid(){
-        if (typeof this._value !== "number") {
-           throw new Error("Is not a Int") 
+    private ensureValueIsUuid(): void {
+        const uuidRegex =
+            /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i
+
+        if (!uuidRegex.test(this._value)) {
+            throw new Error("Value is not a valid UUID")
         }
     }
 }
