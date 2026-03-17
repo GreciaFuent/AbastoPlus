38158feaf7928973ba4780e00dbafbf96cf9b105
Daniela Matul <mayda.matul@gmail.com>
2026-03-17 09:25:44 -0600

    refactor(TranlateService): add service to save product case

--- src/catalog/product/application/use-cases/save-product.ts
+++ src/catalog/product/application/use-cases/save-product.ts
@@ -1,17 +1,21 @@
 import { inject, injectable } from "inversify";
 import Product from "../../domain/product";
 import type ProductRepository from "../productRepository";
+import type TranslateService from "../ports/translate_service";
 
 @injectable()
 class SaveProduct {
     private repository: ProductRepository
+    private translator: TranslateService
 
-    constructor(@inject("ProductRepository") repository: ProductRepository) {
+    constructor(@inject("ProductRepository") repository: ProductRepository, @inject("TranslateService") translator: TranslateService) {
         this.repository = repository;
+        this.translator = translator
     }
 
-    public execute(data: {productId:string, productName: string, baseUnit:string, productPresentation: Array<{id: string, name: string, type: string, netQuantity: number, unitOfMeasure: string}>}): void {
-        const newProduct = Product.build(data.productId, data.productName, data.baseUnit, data.productPresentation)
+    async execute(data: {productId:string, productName: string, baseUnit:string, productPresentation: Array<{id: string, name: string, type: string, netQuantity: number, unitOfMeasure: string}>}): Promise<void>{
+        const newName = await this.translator.translate(data.productName, 'en')
+        const newProduct = await Product.build(data.productId, newName, data.baseUnit, data.productPresentation)
         this.repository.save(newProduct)
     }
 }
