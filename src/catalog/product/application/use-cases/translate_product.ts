2bf0a794e897da35a5b440c7364b48cc35d12e80
Daniela Matul <mayda.matul@gmail.com>
2026-03-24 00:17:51 -0600

    refactor(translate): modify api to translate

--- src/catalog/product/application/use-cases/translate_product.ts
+++ src/catalog/product/application/use-cases/translate_product.ts
@@ -1,17 +1,19 @@
 import type TranslateService from "../ports/translate_service";
 import Product from "../../domain/product";
 import { inject, injectable } from "inversify";
+import { SubscriptionRepository } from "../../../../shared/domain/bus/suscription-repository";
 
 @injectable()
-export default class TranslateProduct{
+export default class TranslateProduct implements  SubscriptionRepository<{ name: string }>{
     private repository: TranslateService
 
     constructor(@inject("TranslateService") repository: TranslateService){
         this.repository = repository
     }
 
-    async execute(name: string): Promise<string> {
-        const algo = await this.repository.translate(name, 'es')
-        return algo
+    async execute(payload: {name: string}): Promise<void>  {
+        const translated = await this.repository.translate(payload.name, 'en')
+        console.log("Nombre traducido", translated)
     }
+
 }
